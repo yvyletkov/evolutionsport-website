3ecf8d691014f6781eef81b7db27fc03d053c7ec
f1tzy <winndini@gmail.com>
2020-09-08 17:25:11 +0300

    add sends form autumn camp

--- js/requests.js
+++ js/requests.js
@@ -25,17 +25,17 @@ $(".children-form").on("submit", function (event) {
       if (res.ok) {
         swal({
           type: "success",
-          title: 'Спасибо! Ваша заявка будет обработана',
-          text: 'Наш менеджер с вами свяжется в указанное время',
-        })
+          title: "Спасибо! Ваша заявка будет обработана",
+          text: "Наш менеджер с вами свяжется в указанное время",
+        });
       }
     })
     .catch((err) => {
       swal({
         type: "error",
-        title: 'Извините, ваша заявка не отправлена',
-        text: 'Что-то пошло не так мы уже работаем над ошибкой'
-      })
+        title: "Извините, ваша заявка не отправлена",
+        text: "Что-то пошло не так мы уже работаем над ошибкой",
+      });
       console.log(err);
     });
 });
@@ -48,17 +48,17 @@ $(".map-form").on("submit", function (event) {
       if (res.ok) {
         swal({
           type: "success",
-          title: 'Спасибо!',
-          text: 'Ваша заявка будет обработана и наш менеджер с вами свяжется',
-        })
+          title: "Спасибо!",
+          text: "Ваша заявка будет обработана и наш менеджер с вами свяжется",
+        });
       }
     })
     .catch((err) => {
       swal({
         type: "error",
-        title: 'Извините, ваша заявка не отправлена',
-        text: 'Что-то пошло не так мы уже работаем над ошибкой'
-      })
+        title: "Извините, ваша заявка не отправлена",
+        text: "Что-то пошло не так мы уже работаем над ошибкой",
+      });
       console.log(err);
     });
 });
@@ -68,21 +68,51 @@ $(".price-form").on("submit", function (event) {
   const data = $(".price-form").serializeArray();
   mainApi(data)
     .then((res) => {
+      console.log(res);
+      res.map((e) => console.log(typeof e.value));
       if (res.ok) {
         swal({
           type: "success",
-          title: 'Спасибо!',
-          text: 'Ваша заявка будет обработана и наш менеджер с вами свяжется',
-        })
+          title: "Спасибо!",
+          text: "Ваша заявка будет обработана и наш менеджер с вами свяжется",
+        });
         this.reset();
       }
     })
     .catch((err) => {
       swal({
         type: "error",
-        title: 'Извините, ваша заявка не отправлена',
-        text: 'Что-то пошло не так мы уже работаем над ошибкой'
-      })
+        title: "Извините, ваша заявка не отправлена",
+        text: "Что-то пошло не так мы уже работаем над ошибкой",
+      });
+      console.log(err);
+    });
+});
+
+$(".tarifes-list").on("submit", function (event) {
+  event.preventDefault();
+  const sum = +$("#new__price-first").text().match(/\d+/g).join("");
+  let obj = [];
+  if ($("p").is(".tarifes-list-item-row__item")) {
+    const childNum = $("#child-num").text();
+    const daysNum = $("#days-num").text();
+    obj = [
+      { name: "form-name", value: "autumn-camp-day" },
+      { name: "price", value: sum },
+      { name: "count-children", value: childNum },
+      { name: "count-days", value: daysNum },
+    ];
+  } else {
+    obj = [
+      { name: "form-name", value: "autumn-camp" },
+      { name: "price", value: sum },
+    ];
+  }
+  mainApi(obj)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
       console.log(err);
     });
 });
