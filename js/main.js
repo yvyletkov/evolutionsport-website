0c277b8526b6fe237fb4bd6553fc2adbc9de0497
f1tzy <winndini@gmail.com>
2020-09-14 20:35:21 +0300

    When you go to home???

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-
-
-// $(document).on("click", "#sidebarCollapse", function (e) {
-//   $(".norm-menu-wrap").toggleClass("active");
-// });
-
-<<<<<<< HEAD
-=======
-// $(document).on("click", ".norm-menu__close", function (e) {
-//   $(".norm-menu-wrap").removeClass("active");
-// });
-//
-// $(".norm-menu-items-item.list-wrap span").click(function (e) {
-//   e.preventDefault();
-//   $(this).closest(".norm-menu-items-item").toggleClass("active");
-//   if ($(".norm-menu-items-item").is(".active")) {
-//     $(".norm-menu-items-item").removeClass("active");
-//     $(this).closest(".norm-menu-items-item").addClass("active");
-//   }
-// });
-
->>>>>>> origin/master
-//дата для бронирования
-// const dateNow = () => {
-//     const today = new Date();
-//     const myDate = document.querySelectorAll('.booking__input');
-//     myDate.forEach((e) => e.value = today.toISOString().substr(0, 10) )
-//   };
-// dateNow();
-
-<<<<<<< HEAD
-// анимации иконки меню
-=======
-// анимации иконки меню
-<<<<<<< HEAD
-=======
-// $("#sidebarCollapse").click(function () {
-//   $(this).toggleClass("active");
-//   $(".norm-menu-wrap").toggleClass("active");
-// });
->>>>>>> origin/master
-
->>>>>>> dev_a
+$(document).ready(
+    function () {
+        $(".nav-link.button-blue__mobile, .nav-link.button-blue").click( () => {
+            swal({
+                title: 'Пожалуйста, укажите свои контактные данные, и мы Вам перезвоним',
+                html:
+                    '<input name="name" id="swal-input1" class="swal2-input" placeholder="Ваше имя">' +
+                    '<input style="width: 100%" name="phone" id="swal-input2" class="swal2-input" placeholder="Номер телефона">',
+                showCancelButton: true,
+                confirmButtonColor: '#ff5908',
+                cancelButtonColor: '#666',
+                confirmButtonText: 'Далее',
+                cancelButtonText: 'Отмена',
+                preConfirm: function () {
+                    return new Promise(function (resolve) {
+                        resolve([
+                            $('#swal-input1').val(),
+                            $('#swal-input2').val(),
+                            $('#swal-textarea1').val()
+                        ])
+                    })
+                },
+            }).then(function (result) {
+                var result = {};
+                result["form-name"] = "quiz-parasport-form";
+                result["rus-form-name"] = "Контактная форма из шапки сайта";
+                result.name = $('#swal-input1').val();
+                result.phone = $('#swal-input2').val();
+                $.ajax({
+                    url:"http://95.181.172.100/erp/site/forms/",
+                    type: "POST",
+                    data: JSON.stringify(result),
+                    error: function(xhr,status,error){
+                        console.log(status);
+                        console.log(error);
+                    }
+                }).done(function() {
+                    Swal({type: 'success', text: 'Сообщение отправлено'});
+                });
+            })
+        })
+    }
+)
