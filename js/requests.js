cf8fa15308f5fd67a256d8fec1804819fa8d9e27
yvyletkov <yavyletkov@ya.ru>
2020-09-21 19:05:25 +0300

    fixes

--- js/requests.js
+++ js/requests.js
@@ -338,12 +338,13 @@ $(document).ready(
 // -------- КОНТАКТНАЯ ФОРМА СО СТРАНИЦЫ BOOKING
 $(document).ready(
     function () {
-        $(".call-me-btn").click(() => {
+        $(".call-me-btn, #call-me-btn").click(() => {
             swal({
                 title: '<p class="header__popup__title">Пожалуйста, укажите свои контактные данные,<br/>и мы Вам перезвоним</p>',
                 html:
                     '<input name="name" id="swal-input1" class="header__popup__input" placeholder="Ваше имя">' +
-                    '<input name="phone" type="number" id="swal-input2" class="header__popup__input" placeholder="Номер телефона">',
+                    '<input name="phone" type="number" id="swal-input2" class="header__popup__input" placeholder="Номер телефона">' +
+                    '<input name="email" type="email" id="swal-input3" class="header__popup__input" placeholder="Ваша почта">',
                 showCancelButton: true,
                 confirmButtonColor: '#355b8e',
                 cancelButtonColor: '#4a4a4a',
@@ -354,16 +355,17 @@ $(document).ready(
                         resolve([
                             $('#swal-input1').val(),
                             $('#swal-input2').val(),
-                            $('#swal-textarea1').val()
+                            $('#swal-input3').val()
                         ])
                     })
                 },
             }).then(function () {
                 let result = {};
                 result["form-name"] = "default-contact-form";
-                result["rus-form-name"] = `Кнопка "Позвоните мне" со страницы /booking`;
+                result["rus-form-name"] = `Кнопка "Позвоните мне"/"Заказать звонок" со страницы "${$('title').html()}"`;
                 result.name = $('#swal-input1').val();
                 result.phone = $('#swal-input2').val();
+                result.email = $('#swal-input3').val();
                 mainApi(result)
                     .then((res) => {
                         console.log(res);
