e8735da2eb2b12b05333bf7df78f88aca4ec9806
yvyletkov <yavyletkov@ya.ru>
2020-09-23 18:50:46 +0300

    index, sport fees, autumn camp +day camp, health-center pages speedBOOSTED

--- header/header.js
+++ header/header.js
@@ -29,3 +29,30 @@ $(".norm-menu-items-item.list-wrap span, #infrastructure-menu-item, #services-me
 $("#sidebarCollapse").click(function () {
     $(".norm-menu-wrap").addClass("active");
 });
+
+
+$(document).ready(function (){
+    debugger
+
+    let YaMapsShown = false;
+
+    $(window).scroll(function() {
+        if (!YaMapsShown){
+            if($(window).scrollTop() + $(window).height() > $(document).height() - 2000) {
+                showYaMaps();
+                YaMapsShown = true;
+            }
+        }
+    });
+
+    function showYaMaps(){
+        let script   = document.createElement("script");
+        script.type  = "text/javascript";
+        script.src   = "https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9a421540de853b065d785f0fc604e13be13cbd2e8f6e69e5346192b568227b5a&amp;width=100%25&amp;height=495&amp;lang=ru_RU&amp;scroll=true";
+        script.async = true;
+        document.getElementById("YaMaps").appendChild(script);
+    }
+
+});
+
+
