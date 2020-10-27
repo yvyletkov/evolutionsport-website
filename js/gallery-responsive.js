2cf0ef17d66674fda73d56a01e81bdebbf05bd83
yvyletkov <yvyletkov@ya.ru>
2020-10-27 11:30:30 +0300

    health center calc forms fixed

--- js/gallery-responsive.js
+++ js/gallery-responsive.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+$(document).ready(function () {
   $(document).ready(function () {
     $(".fancybox").fancybox();
   });
@@ -42,4 +42,4 @@ window.onload = function () {
   $(document).ready(function () {
     $(".fancybox").fancybox();
   });
-};
+})
