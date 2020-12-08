dbcaa21cf55dfdc3262b1bbe8420c468c7f215b6
Vadim Zakirov <vadimzakirov1992@gmail.com>
2020-12-08 17:17:50 +0300

    add search in FAQ

---
+++ js/faq.js
@@ -0,0 +1,23 @@
+
+window.addEventListener('load', () => {
+    const listOfQuestions = Array.prototype.slice.call(document.querySelectorAll('.ac-label'));
+    const findQuestionsPlaceholder = document.getElementById('findQuestions');
+    const searchInput = document.querySelector('#searchInput');
+    searchInput
+        .addEventListener('change', () => {
+            findQuestionsPlaceholder.innerHTML = "";
+            let findQuestions = listOfQuestions.filter(findQ => findQ.innerHTML.toLowerCase().indexOf(searchInput.value.toLowerCase()) !== -1)
+            findQuestions.forEach((value, key, set) => {
+                let answerText = value.parentNode.getElementsByTagName('p')[0];
+                console.log(answerText)
+                findQuestionsPlaceholder.innerHTML += `
+                <div class="card mt-2">
+                     <div class="card-body" style="min-height: auto;padding-top: 20px;">
+                        <h5 class="card-title" style="color:#000">${value.innerHTML}</h5>
+                        ${answerText.innerHTML}
+                     </div>
+                </div>
+                `
+            })
+        })
+})
