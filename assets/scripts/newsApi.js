d9e5779336298ad4b9605dd4539d9a3bd92c2466
yvyletkov <yvyletkov@ya.ru>
2021-02-15 15:48:50 +0300

    connect news db

--- assets/scripts/newsApi.js
+++ assets/scripts/newsApi.js
@@ -1,30 +1,46 @@
 const BD_SERVER_URL = 'https://evo-dashboard.ml/'
 
+function cutTags(str) {
+    const regex = /<[^>]*>/g;
+    return str.replace(regex, "");
+}
+
 window.addEventListener('load', async () => {
     const res = await fetch(`${BD_SERVER_URL}evo/news/list`, {
         method: "GET",
     })
-    const json = await res.json();
-    console.log(json)
 
-    json.forEach(function (item) {
-        document.querySelector("#news-index-slider").insertAdjacentHTML('afterbegin', `<div class="item">
+    if (res.status === 200) {
+        const json = await res.json();
+        console.log(json)
+
+        json.forEach(function (item) {
+
+            if (!item.unactive) document.querySelectorAll("#news-index-slider, #page-news").forEach( (el) => {
+                el.insertAdjacentHTML('afterbegin', `
+                    <div class="item" data-id="${item._id}">
                         <img
                                 class="lazyload img-fluid"
                                 src='${BD_SERVER_URL}${item.previewImg}'
                                 alt=""
                         />
                         <div class="news-details">
-                            ${item.title}
+                            <p>${item.title}</p>
                             <span class="news-subtitle">
-                                ${item.content}
+                                ${cutTags(item.content).slice(0, 100)}
                             </span>
-                            <a href="#" class="btn button-blue">Подробнее</a>
+                            <a href="/new/news.html?id=${item._id}" class="btn button-blue">Подробнее</a>
                         </div>
                     </div>    
     
-    `)
-    })
+            `)
+            })
+
+        })
+    }
+    else {
+        console.log('Error while loading NEWS')
+    }
 
     if ($("#news-index-slider").length) {
         const newsSlider = tns({
@@ -53,4 +69,68 @@ window.addEventListener('load', async () => {
         });
     }
 
+    if ($("#page-news").length) {
+        const newsPageSlider = tns({
+            container: "#page-news",
+            items: 1,
+            gutter: 20,
+            loop: true,
+            nav: false,
+            navPosition: "bottom",
+            preventScrollOnTouch: false,
+            controlsText: [
+                '<img src="../img/arrow-thin-left.png">',
+                '<img src="../img/arrow-thin-right.png">',
+            ],
+            responsive: {
+                768: {
+                    items: 1,
+                },
+                1024: {
+                    items: 3,
+                },
+                1200: {
+                    items: 4,
+                },
+            },
+        });
+    }
+
 })
+
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
+
+const loadNewsItem = async () => {
+    const id = getParameterByName('id')
+    const res = await fetch(`${BD_SERVER_URL}evo/news/get`, {
+        method: "POST",
+        body: JSON.stringify({
+            id: `${id}`
+        }),
+        headers: {'Content-Type': "application/json"}
+    })
+
+    if (res.status === 200) {
+
+        let data = await res.json();
+
+        if (data.link) window.location = data.link
+
+        document.querySelector('#news-item-title').innerHTML = data.title
+        document.querySelector('#news-item-content').innerHTML = data.content
+        document.querySelector('#news-item-img').src = `${BD_SERVER_URL}${data.mainImg}`
+        document.querySelector('title').innerHTML = `${data.title} – ЦС Эволюция`
+    }
+    else {
+        throw new Error('Ошибка загрузки новости')
+    }
+}
