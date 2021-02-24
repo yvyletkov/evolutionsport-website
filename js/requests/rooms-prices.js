8b20be6bb1ad8006ccb000ffadd9b554ce81e7e3
f1tzy <winndini@gmail.com>
2021-02-24 17:09:08 +0300

    add request rooms-prices

---
+++ js/requests/rooms-prices.js
@@ -0,0 +1,11 @@
+const mainPageUrl = "http://erp-evo.ml:1337/resort-prices";
+
+requestInfo(mainPageUrl)
+    .then((res) => {
+        $("#peaceful-price").text(`от ${res[0].peaceful} руб.`);
+        $("#borderline-price").text(`от ${res[0].borderline} руб.`);
+        $("#athletic-price").text(`от ${res[0].athletic} руб.`);
+        $("#starry-price").text(`от ${res[0].starry} руб.`);
+        $("#stormy").text(`от ${res[0].stormy} руб.`);
+    })
+    .catch((err) => console.log(err))
