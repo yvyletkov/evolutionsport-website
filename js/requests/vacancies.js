bed248e4218e51017df780fc4a301fc6041f6fcb
f1tzy <winndini@gmail.com>
2021-03-09 11:38:20 +0300

    fix slider and requests on vacancy page

--- js/requests/vacancies.js
+++ js/requests/vacancies.js
@@ -18,9 +18,9 @@ async function requestInfo(url) {
 
 requestInfo('vacancies')
     .then((res) => {
-        console.log(res)
         res.forEach((vacancy) => {
-            $(`
+            if (vacancy.type === "food") {
+                $(`
            <div class="vacancy-block-tabs-tab-slider-item">
                                 <div class="vacancy-block-tabs-tab-slider-item-info">
                                     <div class="row vacancy-block-tabs-tab-slider-item-info__text">
@@ -36,18 +36,15 @@ requestInfo('vacancies')
                                             <p>Зарплата: ${vacancy.salary}</p>
                                             <p>Должностные обязанности:</p>
                                             <ul>
-                                            ${vacancy.duties.duties ? 
-                                                vacancy.duties.duties.map((e) => {
-                                                    console.log(e)
-                                                    return `<li>${e}</li>`
-                                                })
-                                                : ''}
+                                            ${vacancy.duties.duties ?
+                    vacancy.duties.duties.map((e) => `<li>${e}</li>`).join("")
+                    : ''}
                                             </ul>
                                             <p>Требования:</p>
                                             <ul>
                                             ${vacancy.duties.demads ?
-                                                vacancy.duties.demads.map((e) => `<li>${e}</li>`)
-                                            : ''}
+                    vacancy.duties.demads.map((e) => `<li>${e}</li>`).join("")
+                    : ''}
                                             </ul>
                                             <a class="btn btn-primary"
                                                style="background-color:#4273b3;border-color:#4273b3;"
@@ -60,6 +57,39 @@ requestInfo('vacancies')
 
                             </div>
             `).appendTo('#food');
+            }
+        })
+        $(document).on('click', '.tabs-navigation-item', function (e) {
+            e.preventDefault();
+            let tabContainers = $(this).closest('.tabs').find('.tabs-tab');
+            tabContainers.removeClass('active');
+            tabContainers.filter(this.hash).addClass('active');
+            $('.tabs-navigation-item').removeClass('active');
+            $(this).addClass('active');
+            $('.vacancy-block-tabs-tab-slider.slick-initialized').slick('unslick');
+            $('.vacancy-block-tabs-tab-slider').slick({
+                arrows: false,
+                dots: false,
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            });
+        });
+        $(document).ready(function () {
+            $('.vacancy-block-tabs-tab-slider').slick({
+                arrows: false,
+                dots: false,
+                infinite: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            });
+        });
+
+        $(document).on('click', '.vacancy-block-tabs-tab .supaSlider-insideSliderBlock__next', function () {
+            $(this).closest('.vacancy-block-tabs-tab').find('.vacancy-block-tabs-tab-slider').slick('slickNext');
+        })
+        $(document).on('click', '.vacancy-block-tabs-tab .supaSlider-insideSliderBlock__prev', function () {
+            $(this).closest('.vacancy-block-tabs-tab').find('.vacancy-block-tabs-tab-slider').slick('slickPrev');
         })
     })
     .catch((err) => console.log(err))
